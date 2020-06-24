334e6fb691405837e787d57e904703b74854320f
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2020-06-24 17:37:18 +0900

    change variable in js
    yes

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
-let y = Math.floor(Math.random()*10+1);
-//random한 숫자를 주기 위한 변수설정 . Math 이용 숫자 혹시 바뀔수도 있으니 let으로 설정.
+const y = Math.floor(Math.random()*10+1);
 const guess = 1;
 
 document.querySelector("#submitguess").onclick = function(){
-    var x = document.querySelector(".guessField").value;
+    const x = document.querySelector(".guessField").value;
 
 if(x == y)
 {
-    alert(`Congratualtion you guess ${guess} right way`);
+    alert(`Congratualtion you guess right way The number is ${x} ! `);
 }
 else if(x > y)
 {
 
-    alert("sorry try a smaller number");
+    alert("Sorry try a smaller number");
 }
 else
 {
 
-    alert("sorry try a greater number");
+    alert("Sorry try a greater number");
 }
     };
 
